d1490fa6cebbabb2036f32251aa89fe8a8bd352f
1463353697 <1463353697@qq.com>
2018-10-21 12:59:40 +0800

    first commit

---
+++ 网页跳转test/跳转js.js
@@ -0,0 +1,20 @@
+// window.onload = function () {
+// 	var button = document.getElementById('b');
+// 	var input = document.getElementById('i');
+// 	var div1 = document.getElementById('div1');
+// 	var div2 = document.getElementById('div2');
+// 	var inputValue = input.value;
+// 	if (inputValue == "1") {
+// 		div1.style.display = "none";
+// 	}
+// 	button.onclick = function () {
+// 		window.open("跳转2.html",true);
+			
+// 	}
+// 	var inputValue = input.value;
+// 	if (inputValue == "1") {
+// 		div1.style.display = "none";
+// 	}
+
+	
+// }
